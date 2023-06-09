850c3e1bb94a38efc5e3dce4585da2b2cd98a007
jyi2ya <jyi2ya@outlook.com>
2023-06-09 15:18:24 +0800

    fix: memory leak

--- tests/ldu-to-csc.cpp
+++ tests/ldu-to-csc.cpp
@@ -1,19 +1,19 @@
+#include <cassert>
 #include <cstdio>
 #include <cstdlib>
-#include <cassert>
 #include <cstring>
 #include <ctime>
 #include <vector>
 
-#include "../pcg_def.h"
-#include "../pcg.h"
+#include "pcg.h"
+#include "pcg_def.h"
 
-void csr_to_csc(const CsrMatrix &csr_matrix, CscMatrix &csc_matrix) {
+void csr_to_csc(const CsrMatrix& csr_matrix, CscMatrix& csc_matrix) {
     csc_matrix.cols = csr_matrix.rows;
     csc_matrix.data_size = csr_matrix.data_size;
-    csc_matrix.col_off = (int *)malloc((csc_matrix.cols + 1)*sizeof(int));
-    csc_matrix.rows = (int *)malloc(csc_matrix.data_size*sizeof(int));
-    csc_matrix.data = (double *)malloc(csc_matrix.data_size*sizeof(double));
+    csc_matrix.col_off = (int*)malloc((csc_matrix.cols + 1) * sizeof(int));
+    csc_matrix.rows = (int*)malloc(csc_matrix.data_size * sizeof(int));
+    csc_matrix.data = (double*)malloc(csc_matrix.data_size * sizeof(double));
 
     for (int i = 0; i < csc_matrix.cols + 1; i++) {
         csc_matrix.col_off[i] = 0;
@@ -23,14 +23,15 @@ void csr_to_csc(const CsrMatrix &csr_matrix, CscMatrix &csc_matrix) {
         csc_matrix.col_off[csr_matrix.cols[i] + 1]++;
     }
 
-    int last_column_size = csc_matrix.col_off[csc_matrix.cols -1];
+    int last_column_size = csc_matrix.col_off[csc_matrix.cols - 1];
 
     for (int i = 0; i < csc_matrix.cols; i++) {
         csc_matrix.col_off[i + 1] += csc_matrix.col_off[i];
     }
 
     for (int i = 0; i < csr_matrix.rows; i++) {
-        for (int j = csr_matrix.row_off[i]; j < csr_matrix.row_off[i + 1]; j++) {
+        for (int j = csr_matrix.row_off[i]; j < csr_matrix.row_off[i + 1];
+             j++) {
             int col = csr_matrix.cols[j];
             int dest = csc_matrix.col_off[col];
 
@@ -41,39 +42,43 @@ void csr_to_csc(const CsrMatrix &csr_matrix, CscMatrix &csc_matrix) {
         }
     }
     for (int i = 0; i < csc_matrix.cols; i++) {
-        csc_matrix.col_off[i] -= csc_matrix.col_off[i + 1] - csc_matrix.col_off[i];
+        csc_matrix.col_off[i] -=
+            csc_matrix.col_off[i + 1] - csc_matrix.col_off[i];
     }
     csc_matrix.col_off[csc_matrix.cols - 1] -= last_column_size;
 }
 
-void ldu_to_csc(const LduMatrix &ldu_matrix, CscMatrix &csc_matrix) {
+void ldu_to_csc(const LduMatrix& ldu_matrix, CscMatrix& csc_matrix) {
     csc_matrix.cols = ldu_matrix.cells;
-    csc_matrix.data_size = 2*ldu_matrix.faces + ldu_matrix.cells;
-    csc_matrix.col_off = (int *)malloc((csc_matrix.cols + 1)*sizeof(int));
-    csc_matrix.rows = (int *)malloc(csc_matrix.data_size*sizeof(int));
-    csc_matrix.data = (double *)malloc(csc_matrix.data_size*sizeof(double));
+    csc_matrix.data_size = 2 * ldu_matrix.faces + ldu_matrix.cells;
+    csc_matrix.col_off = (int*)malloc((csc_matrix.cols + 1) * sizeof(int));
+    csc_matrix.rows = (int*)malloc(csc_matrix.data_size * sizeof(int));
+    csc_matrix.data = (double*)malloc(csc_matrix.data_size * sizeof(double));
 
     int row, col, offset;
-    int *tmp = (int *)malloc((csc_matrix.cols + 1)*sizeof(int));
+    int* tmp = (int*)malloc((csc_matrix.cols + 1) * sizeof(int));
 
     csc_matrix.col_off[0] = 0;
-    for(int i = 1; i < csc_matrix.cols + 1; i++)
+    for (int i = 1; i < csc_matrix.cols + 1; i++)
         csc_matrix.col_off[i] = 1;
 
-    for(int i = 0; i < ldu_matrix.faces; i++){
-        row     = ldu_matrix.uPtr[i] ;
-        col = ldu_matrix.lPtr[i] ;
-        csc_matrix.col_off[row+1]++;
-        csc_matrix.col_off[col+1]++;
+    for (int i = 0; i < ldu_matrix.faces; i++) {
+        row = ldu_matrix.uPtr[i];
+        col = ldu_matrix.lPtr[i];
+        csc_matrix.col_off[row + 1]++;
+        csc_matrix.col_off[col + 1]++;
     }
 
-    for(int i = 0;i< ldu_matrix.cells; i++){
-        csc_matrix.col_off[i+1] += csc_matrix.col_off[i];
+    for (int i = 0; i < ldu_matrix.cells; i++) {
+        csc_matrix.col_off[i + 1] += csc_matrix.col_off[i];
     }
 
-    memcpy(&tmp[0], &csc_matrix.col_off[0], (ldu_matrix.cells + 1)*sizeof(int));
+    memcpy(
+        &tmp[0],
+        &csc_matrix.col_off[0],
+        (ldu_matrix.cells + 1) * sizeof(int));
     // lower
-    for(int i = 0; i < ldu_matrix.faces; i++ ){
+    for (int i = 0; i < ldu_matrix.faces; i++) {
         row = ldu_matrix.uPtr[i];
         col = ldu_matrix.lPtr[i];
         offset = tmp[col]++;
@@ -82,14 +87,14 @@ void ldu_to_csc(const LduMatrix &ldu_matrix, CscMatrix &csc_matrix) {
     }
 
     // diag
-    for(int i = 0; i < ldu_matrix.cells; i++){
+    for (int i = 0; i < ldu_matrix.cells; i++) {
         offset = tmp[i]++;
         csc_matrix.rows[offset] = i;
         csc_matrix.data[offset] = ldu_matrix.diag[i];
     }
 
     // upper
-    for(int i = 0; i < ldu_matrix.faces; i++){
+    for (int i = 0; i < ldu_matrix.faces; i++) {
         row = ldu_matrix.lPtr[i];
         col = ldu_matrix.uPtr[i];
         offset = tmp[col]++;
@@ -100,34 +105,38 @@ void ldu_to_csc(const LduMatrix &ldu_matrix, CscMatrix &csc_matrix) {
     free(tmp);
 }
 
-void ldu_to_csr(const LduMatrix &ldu_matrix, CsrMatrix &csr_matrix) {
+void ldu_to_csr(const LduMatrix& ldu_matrix, CsrMatrix& csr_matrix) {
     csr_matrix.rows = ldu_matrix.cells;
-    csr_matrix.data_size = 2*ldu_matrix.faces + ldu_matrix.cells;
-    csr_matrix.row_off = (int *)malloc((csr_matrix.rows + 1)*sizeof(int));
-    csr_matrix.cols = (int *)malloc(csr_matrix.data_size*sizeof(int));
-    csr_matrix.data = (double *)malloc(csr_matrix.data_size*sizeof(double));
+    csr_matrix.data_size = 2 * ldu_matrix.faces + ldu_matrix.cells;
+    csr_matrix.row_off = (int*)malloc((csr_matrix.rows + 1) * sizeof(int));
+    csr_matrix.cols = (int*)malloc(csr_matrix.data_size * sizeof(int));
+    csr_matrix.data = (double*)malloc(csr_matrix.data_size * sizeof(double));
 
     int row, col, offset;
-    int *tmp = (int *)malloc((csr_matrix.rows + 1)*sizeof(int));
+    int* tmp = (int*)malloc((csr_matrix.rows + 1) * sizeof(int));
 
     csr_matrix.row_off[0] = 0;
-    for(int i = 1; i < csr_matrix.rows + 1; i++)
+    for (int i = 1; i < csr_matrix.rows + 1; i++)
         csr_matrix.row_off[i] = 1;
 
-    for(int i = 0; i < ldu_matrix.faces; i++){
-        row	= ldu_matrix.uPtr[i] ;
-        col = ldu_matrix.lPtr[i] ;
-        csr_matrix.row_off[row+1]++;
-        csr_matrix.row_off[col+1]++;
+    for (int i = 0; i < ldu_matrix.faces; i++) {
+        row = ldu_matrix.uPtr[i];
+        col = ldu_matrix.lPtr[i];
+        csr_matrix.row_off[row + 1]++;
+        csr_matrix.row_off[col + 1]++;
     }
 
-    for(int i = 0;i< ldu_matrix.cells; i++){
-        csr_matrix.row_off[i+1] += csr_matrix.row_off[i];
+    for (int i = 0; i < ldu_matrix.cells; i++) {
+        csr_matrix.row_off[i + 1] += csr_matrix.row_off[i];
     }
 
-    memcpy(&tmp[0], &csr_matrix.row_off[0], (ldu_matrix.cells + 1)*sizeof(int));
+    memcpy(
+        &tmp[0],
+        &csr_matrix.row_off[0],
+        (ldu_matrix.cells + 1) * sizeof(int));
+
     // lower
-    for(int i = 0; i < ldu_matrix.faces; i++ ){
+    for (int i = 0; i < ldu_matrix.faces; i++) {
         row = ldu_matrix.uPtr[i];
         col = ldu_matrix.lPtr[i];
         offset = tmp[row]++;
@@ -136,14 +145,14 @@ void ldu_to_csr(const LduMatrix &ldu_matrix, CsrMatrix &csr_matrix) {
     }
 
     // diag
-    for(int i = 0; i < ldu_matrix.cells; i++){
+    for (int i = 0; i < ldu_matrix.cells; i++) {
         offset = tmp[i]++;
         csr_matrix.cols[offset] = i;
         csr_matrix.data[offset] = ldu_matrix.diag[i];
     }
 
     // upper
-    for(int i = 0; i < ldu_matrix.faces; i++){
+    for (int i = 0; i < ldu_matrix.faces; i++) {
         row = ldu_matrix.lPtr[i];
         col = ldu_matrix.uPtr[i];
         offset = tmp[row]++;
@@ -162,14 +171,14 @@ double rand_kernel(void) {
     return (double)rand() / RAND_MAX * 10;
 }
 
-void generate_ldu_matrix(LduMatrix &ldu_matrix, int size) {
+void generate_ldu_matrix(LduMatrix& ldu_matrix, int size) {
     ldu_matrix.cells = size;
     ldu_matrix.faces = size * (size - 1) / 2;
-    ldu_matrix.lPtr = (int *)malloc(ldu_matrix.faces * sizeof(int));
-    ldu_matrix.uPtr = (int *)malloc(ldu_matrix.faces * sizeof(int));
-    ldu_matrix.lower = (double *)malloc(ldu_matrix.faces * sizeof(double));
-    ldu_matrix.upper = (double *)malloc(ldu_matrix.faces * sizeof(double));
-    ldu_matrix.diag = (double *)malloc(ldu_matrix.cells * sizeof(double));
+    ldu_matrix.lPtr = (int*)malloc(ldu_matrix.faces * sizeof(int));
+    ldu_matrix.uPtr = (int*)malloc(ldu_matrix.faces * sizeof(int));
+    ldu_matrix.lower = (double*)malloc(ldu_matrix.faces * sizeof(double));
+    ldu_matrix.upper = (double*)malloc(ldu_matrix.faces * sizeof(double));
+    ldu_matrix.diag = (double*)malloc(ldu_matrix.cells * sizeof(double));
 
     int k = 0;
     for (int i = 0; i < size; i++) {
@@ -184,7 +193,7 @@ void generate_ldu_matrix(LduMatrix &ldu_matrix, int size) {
     }
 }
 
-std::vector<double> plain_matrix_from_csr(const CsrMatrix &csr_matrix) {
+std::vector<double> plain_matrix_from_csr(const CsrMatrix& csr_matrix) {
     std::vector<double> result;
 
     for (int i = 0; i < csr_matrix.rows; i++) {
@@ -202,7 +211,7 @@ std::vector<double> plain_matrix_from_csr(const CsrMatrix &csr_matrix) {
     return result;
 }
 
-std::vector<double> plain_matrix_from_csc(const CscMatrix &csc_matrix) {
+std::vector<double> plain_matrix_from_csc(const CscMatrix& csc_matrix) {
     std::vector<double> result;
 
     for (int i = 0; i < csc_matrix.cols; i++) {
@@ -226,13 +235,15 @@ std::vector<double> plain_matrix_from_csc(const CscMatrix &csc_matrix) {
     return result;
 }
 
-bool csc_check(const CscMatrix &lhs, const CscMatrix &rhs) {
-#define DO_CHECK(a, b) do { \
-    if ((a) != (b)) { \
-        fprintf(stderr, "[ERR] %s != %s\n", #a, #b); \
-        return false; \
-    } \
-} while (0)
+#if 0
+bool csc_check(const CscMatrix& lhs, const CscMatrix& rhs) {
+    #define DO_CHECK(a, b) \
+        do { \
+            if ((a) != (b)) { \
+                fprintf(stderr, "[ERR] %s != %s\n", #a, #b); \
+                return false; \
+            } \
+        } while (0)
 
     DO_CHECK(lhs.cols, rhs.cols);
     DO_CHECK(lhs.data_size, rhs.data_size);
@@ -248,6 +259,27 @@ bool csc_check(const CscMatrix &lhs, const CscMatrix &rhs) {
 
     return true;
 }
+#endif
+
+void free_csr_matrix(CsrMatrix& mtx) {
+    free(mtx.cols);
+    free(mtx.data);
+    free(mtx.row_off);
+}
+
+void free_csc_matrix(CscMatrix& mtx) {
+    free(mtx.rows);
+    free(mtx.data);
+    free(mtx.col_off);
+}
+
+void free_ldu_matrix(LduMatrix& mtx) {
+    free(mtx.upper);
+    free(mtx.lower);
+    free(mtx.diag);
+    free(mtx.lPtr);
+    free(mtx.uPtr);
+}
 
 int main(void) {
     srand(time(NULL));
@@ -256,16 +288,28 @@ int main(void) {
     CsrMatrix csr_matrix;
     CscMatrix csc_matrix_from_ldu;
     CscMatrix csc_matrix_from_csr;
-    generate_ldu_matrix(ldu_matrix, 5);
-    ldu_to_csr(ldu_matrix, csr_matrix);
-    ldu_to_csc(ldu_matrix, csc_matrix_from_ldu);
-    csr_to_csc(csr_matrix, csc_matrix_from_csr);
 
-    auto plain_csc1 = plain_matrix_from_csc(csc_matrix_from_ldu);
-    auto plain_csc2 = plain_matrix_from_csc(csc_matrix_from_csr);
-    auto plain_csr = plain_matrix_from_csr(csr_matrix);
+    for (int _i = 0; _i < 25; ++_i) {
+        int matrix_size = (_i + 1) * 5;
+        fprintf(stderr, "[INFO] matrix size = %d\n", matrix_size);
+        generate_ldu_matrix(ldu_matrix, matrix_size);
+        ldu_to_csr(ldu_matrix, csr_matrix);
+        ldu_to_csc(ldu_matrix, csc_matrix_from_ldu);
+        csr_to_csc(csr_matrix, csc_matrix_from_csr);
 
-    assert(plain_csc1 == plain_csc2 && plain_csc1 == plain_csr && plain_csr == plain_csc2);
+        auto plain_csc1 = plain_matrix_from_csc(csc_matrix_from_ldu);
+        auto plain_csc2 = plain_matrix_from_csc(csc_matrix_from_csr);
+        auto plain_csr = plain_matrix_from_csr(csr_matrix);
+
+        free_csr_matrix(csr_matrix);
+        free_csc_matrix(csc_matrix_from_csr);
+        free_csc_matrix(csc_matrix_from_ldu);
+        free_ldu_matrix(ldu_matrix);
+
+        assert(
+            plain_csc1 == plain_csc2 && plain_csc1 == plain_csr
+            && plain_csr == plain_csc2);
+    }
 
     return 0;
 }
