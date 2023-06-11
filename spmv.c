750d6a4bef9f65d5fdb3b7fae4eb21c560f489d1
Wenda Zhang <vaaandark@gmail.com>
2023-06-11 17:26:41 +0800

    refactor: split the macro definitions from code

--- spmv.c
+++ spmv.c
@@ -2,35 +2,17 @@
 
 #include <crts.h>
 #include <slave.h>
+#include "slave_def.h"
 
 #include "spmv_def.h"
+
+#include "slave_def.h"
 
 __thread_local crts_rply_t DMARply = 0;
 __thread_local unsigned int DMARplyCount = 0;
 
 #define BUF_ITEM 1024
 __thread_local double redu_buf[BUF_ITEM] __attribute__((aligned(64)));
-
-/// 非阻塞 DMA 读
-#define DMA_IGET(dst, src, len, rply) \
-    CRTS_dma_iget(dst, src, len, rply); \
-    DMARplyCount++
-
-/// DMA 等待完成
-#define DMA_WAIT CRTS_dma_wait_value(&DMARply, DMARplyCount)
-
-/// 阻塞 DMA 读
-#define DMA_GET(dst, src, len, rply) \
-    CRTS_dma_iget(dst, src, len, rply); \
-    DMARplyCount++; \
-    DMA_WAIT
-
-/// 阻塞 DMA 写
-#define DMA_PUT(dst, src, len, rply) \
-    CRTS_dma_iput(dst, src, len, rply); \
-    DMARplyCount++; \
-    DMA_WAIT
-
 void csc_spmv_slave(void *para) {
     SpmvPara spmv_para;
     DMA_GET(&spmv_para, para, sizeof(SpmvPara), &DMARply);
