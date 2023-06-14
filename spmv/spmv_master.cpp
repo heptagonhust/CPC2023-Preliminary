2ddfc85b6b9c7be55cabc630e54ec2fc10d135cc
Eigensystem <eigensystem1318@gmail.com>
2023-06-15 00:21:29 +0800

    feat: polling to reduce slave spmv result

--- spmv/spmv_master.cpp
+++ spmv/spmv_master.cpp
@@ -1,4 +1,8 @@
 #include "spmv_master.h"
+#include <sys/_types/_int64_t.h>
+#include <sys/_types/_size_t.h>
+#include <sys/_types/_u_int64_t.h>
+#include <cstring>
 
 #define SLAVE_CORE_NUM 64
 void csc_spmv(SpmvPara *para, double *result) {
@@ -8,23 +12,31 @@ void csc_spmv(SpmvPara *para, double *result) {
     int *dma_over = para->dma_over;
     CscChunk *chunk0 = para->chunks[0];
     int block_num_per_chunk = chunk0->block_num;
+    int reduced_cnt = 0;
+    int pool_offset = 0;
+    bool reduce_status[chunk_num];
 
     for (int i = 0; i < block_num_per_chunk; ++i) {
         // 每次规约前要等待所有从核 DMA 完成
-        for (int j = 0; j < chunk_num; ++j) {
-            while (dma_over[j] < i) {
-                // wait for dma
-            }
-        }
-        // 开始规约
         CscBlock *block = chunk0->blocks + i;
         int row_begin = block->row_begin;
         int row_num = block->row_end - row_begin;
-        for (int j = 0; j < row_num; ++j, ++result) {
-            for (int k = 0; k < chunk_num; ++k, ++result_pool) {
-                *result += *result_pool;
+
+        reduced_cnt = 0;
+        memset(&reduce_status, 0, chunk_num * sizeof(bool));
+        while (reduced_cnt < chunk_num) {
+            for (int j = 0; j < chunk_num; ++j) {
+                if (dma_over[j] >= i && !reduce_status[j]) {
+                    // wait for dma
+                    for (int k = 0; k < row_num; ++k, ++result) {
+                        result[row_begin + k] += result_pool[pool_offset + k * chunk_num + j];
+                    }
+                    reduce_status[j] = 1;
+                    ++reduced_cnt;
+                }
             }
         }
+        pool_offset += row_num * chunk_num;
     }
 }
 
