3c2fc963ed04ad2c0555118c01837709341a0ba2
Wenda Zhang <vaaandark@gmail.com>
2023-06-15 00:28:43 +0800

    feat: polling in the reduce on master

--- spmv/spmv_master.cpp
+++ spmv/spmv_master.cpp
@@ -3,32 +3,53 @@
 #define SLAVE_CORE_NUM 64
 void csc_spmv(SpmvPara *para, double *result) {
     CRTS_athread_spawn(slave_csc_spmv, &para);
+
+    memset(result, 0, sizeof(double) * para->sp_row);
+
     int chunk_num = para->chunk_num;
     double *result_pool = para->result;
     int *dma_over = para->dma_over;
     CscChunk *chunk0 = para->chunks[0];
     int block_num_per_chunk = chunk0->block_num;
 
+    bool *chunk_over = (bool *)malloc(sizeof(bool) * chunk_num);
+    memset(chunk_over, 0, sizeof(bool) * 64);
     for (int i = 0; i < block_num_per_chunk; ++i) {
-        // 每次规约前要等待所有从核 DMA 完成
-        for (int j = 0; j < chunk_num; ++j) {
-            while (dma_over[j] < i) {
-                // wait for dma
-            }
-        }
-        // 开始规约
         CscBlock *block = chunk0->blocks + i;
         int row_begin = block->row_begin;
         int row_num = block->row_end - row_begin;
-        for (int j = 0; j < row_num; ++j, ++result) {
-            for (int k = 0; k < chunk_num; ++k, ++result_pool) {
-                *result += *result_pool;
+
+        double *result_slice = result + row_begin;
+        double *chunk_result_base = result_pool + chunk_num * row_begin;
+        for (;;) {
+            bool get_out = true;
+            for (int j = 0; j < chunk_num; ++j) {
+                if (dma_over[j] >= i) {
+                    double *chunk_result = chunk_result_base + j;
+                    for (int k = 0; k < row_num; ++k) {
+                        result_slice[k] += *(chunk_result + k * chunk_num);
+                    }
+                    chunk_over[j] = true;
+                }
+                get_out &= chunk_over[j];
+            }
+
+            if (get_out) {
+                break;
             }
         }
+
+        memset(chunk_over, 0, sizeof(bool) * 64);
     }
+
+    free(chunk_over);
 }
 
-void splited_csc_matrix_to_spmv_para(const SplitedCscMatrix *mat, SpmvPara *para, int row_num, int col_num) {
+void splited_csc_matrix_to_spmv_para(
+    const SplitedCscMatrix *mat,
+    SpmvPara *para,
+    int row_num,
+    int col_num) {
     int chunk_num = mat->chunk_num;
     para->chunk_num = chunk_num;
     para->chunks = mat->chunks;
