c680fcc8b2888ad7946136a41e59e512c8eaf805
Wenda Zhang <vaaandark@gmail.com>
2023-06-14 22:50:40 +0800

    fix: add std headers

--- spmv/spmv_master.h
+++ spmv/spmv_master.h
@@ -4,6 +4,8 @@
 #include "pcg_def.h"
 #include "spmv_def.h"
 #include "spmv_slave.h"
+#include <cstdlib>
+#include <cstring>
 
 /// 并行计算稀疏矩阵乘向量(spmv)
 void csc_spmv(const CscMatrix *csc_matrix, const double *vec, double *result);
