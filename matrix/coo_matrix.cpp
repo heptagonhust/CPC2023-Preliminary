9876479e89fedce0e4a9c2b3bc5918a92bb25680
jyi2ya <jyi2ya@outlook.com>
2023-06-23 02:03:44 +0800

    perf: no more binary searches.

--- matrix/coo_matrix.cpp
+++ matrix/coo_matrix.cpp
@@ -115,19 +115,7 @@ static bool real_get_chunk_ranges(
     return true;
 }
 
-struct {
-    int num;
-    int col_begin[64];
-    int col_end[64];
-
-    int data[99999];
-} BSCache;
-
-void init_bs_cache(void) {
-    memset(BSCache.data, -1, sizeof(BSCache.data));
-}
-
-static void get_chunk_ranges_and_init_bs_cache(
+static void get_chunk_ranges(
     CooChunkRange *ranges,
     int *col_size,
     int total_size,
@@ -162,7 +150,6 @@ static void get_chunk_ranges_and_init_bs_cache(
         col_num,
         chunk_num,
         last_max_chunk_size);
-    init_bs_cache();
 }
 
 struct Element {
@@ -181,133 +168,6 @@ struct Element {
 #endif
 };
 
-#ifdef USE_SIMD
-    #include <immintrin.h>
-
-void println256(const char *info, __m256i vec) {
-    int *arr = (int *)&vec;
-    fprintf(stderr, "%s ", info);
-    for (int i = 0; i < 8; ++i) {
-        fprintf(stderr, "%d ", arr[i]);
-    }
-    fprintf(stderr, "\n");
-}
-
-void simd_binary_search(
-    int *res,
-    int *col_begin,
-    int *col_end,
-    int size,
-    int *keys) {
-    __m256i one = _mm256_set1_epi32(1);
-    __m256i all_ones = _mm256_set1_epi32(-1);
-
-    __m256i key = _mm256_loadu_si256((__m256i *)keys);
-    __m256i left = _mm256_set1_epi32(0);
-    __m256i right = _mm256_set1_epi32(size);
-    __m256i result = _mm256_set1_epi32(-1);
-    __m256i mask3 = _mm256_set1_epi32(0);
-
-    while (_mm256_movemask_epi8(mask3) != -1) {
-        __m256i mid = _mm256_add_epi32(
-            left,
-            _mm256_srai_epi32(_mm256_sub_epi32(right, left), 1));
-        __m256i begin = _mm256_i32gather_epi32(col_begin, mid, sizeof(int));
-        __m256i end = _mm256_i32gather_epi32(col_end, mid, sizeof(int));
-        __m256i mask1 =
-            _mm256_xor_si256(_mm256_cmpgt_epi32(end, key), all_ones);
-        __m256i mask2 = _mm256_cmpgt_epi32(begin, key);
-        mask3 = _mm256_xor_si256(_mm256_or_si256(mask1, mask2), all_ones);
-        left = _mm256_blendv_epi8(left, _mm256_add_epi32(mid, one), mask1);
-        right = _mm256_blendv_epi8(right, mid, mask2);
-        result = _mm256_blendv_epi8(result, mid, mask3);
-    }
-
-    _mm256_storeu_si256((__m256i *)res, result);
-}
-
-#else
-
-void naive_binary_search(
-    int *res,
-    int *col_begin,
-    int *col_end,
-    int size,
-    int *keys) {
-    for (int i = 0; i < 8; ++i) {
-        int l = 0, r = size;
-        for (;;) {
-            int m = l + (r - l) / 2;
-            auto begin = col_begin[m];
-            auto end = col_end[m];
-            if (end <= keys[i]) {
-                l = m + 1;
-            } else if (keys[i] < begin) {
-                r = m;
-            } else {
-                res[i] = m;
-                break;
-            }
-        }
-    }
-}
-#endif
-
-void cached_binary_search(
-    int *res,
-    int *col_begin,
-    int *col_end,
-    int size,
-    int *keys) {
-    for (int i = 0; i < 8; ++i) {
-        if (BSCache.data[keys[i]] >= 0) {
-            res[i] = BSCache.data[keys[i]];
-        } else {
-            int l = 0, r = size;
-            for (;;) {
-                int m = l + (r - l) / 2;
-                auto begin = col_begin[m];
-                auto end = col_end[m];
-                if (end <= keys[i]) {
-                    l = m + 1;
-                } else if (keys[i] < begin) {
-                    r = m;
-                } else {
-                    res[i] = m;
-                    break;
-                }
-            }
-            BSCache.data[keys[i]] = res[i];
-        }
-    }
-}
-
-int cached_binary_search_single(
-    int *col_begin,
-    int *col_end,
-    int size,
-    int key) {
-    if (BSCache.data[key] >= 0) {
-        return BSCache.data[key];
-    } else {
-        int l = 0, r = size;
-        for (;;) {
-            int m = l + (r - l) / 2;
-            auto begin = col_begin[m];
-            auto end = col_end[m];
-            if (end <= key) {
-                l = m + 1;
-            } else if (key < begin) {
-                r = m;
-            } else {
-                BSCache.data[key] = m;
-                return m;
-                break;
-            }
-        }
-    }
-}
-
 extern void free_splited_coo(SplitedCooMatrix &mtx) {
     free(mtx.chunk_ranges);
     for (int i = 0; i < mtx.chunk_num; ++i) {
@@ -325,14 +185,15 @@ ldu_to_splited_coo(const LduMatrix &ldu_matrix, int chunk_num) {
     SplitedCooMatrix result;
     int row_num = ldu_matrix.cells;
 
-    int *mem = (int *)malloc(std::max(
-        sizeof(int) * ldu_matrix.cells,
-        sizeof(int) * (chunk_num * 2 + chunk_num * chunk_num * 2)));
+    int *mem = (int *)malloc(
+        sizeof(int)
+        * (ldu_matrix.cells + chunk_num * 2 + chunk_num * chunk_num * 2));
     int *col_begin = mem;
     int *col_end = mem + chunk_num;
     int *chunk_block_off = mem + chunk_num * 2;
     int *block_size = mem + chunk_num * 2 + chunk_num * chunk_num;
-    int *row_size = mem;
+    int *row_size = mem + chunk_num * 2 + chunk_num * chunk_num * 2;
+    int *bscache = mem + chunk_num * 2 + chunk_num * chunk_num * 2;
 
     result.chunk_num = chunk_num;
     result.chunk_ranges =
@@ -351,7 +212,7 @@ ldu_to_splited_coo(const LduMatrix &ldu_matrix, int chunk_num) {
         total_size += row_size[i];
     }
 
-    get_chunk_ranges_and_init_bs_cache(
+    get_chunk_ranges(
         result.chunk_ranges,
         row_size,
         total_size,
@@ -392,28 +253,20 @@ ldu_to_splited_coo(const LduMatrix &ldu_matrix, int chunk_num) {
     }
 
     memset(block_size, 0, sizeof(int) * chunk_num * chunk_num);
-    for (int i = 0; i < ldu_matrix.faces; ++i) {
-        int chunk_idx = cached_binary_search_single(
-            col_begin,
-            col_end,
-            result.chunk_num,
-            ldu_matrix.uPtr[i]);
-        int block_idx = cached_binary_search_single(
-            col_begin,
-            col_end,
-            result.chunk_num,
-            ldu_matrix.lPtr[i]);
-        block_size[chunk_idx * result.chunk_num + block_idx] += 1;
-        block_size[block_idx * result.chunk_num + chunk_idx] += 1;
+
+    for (int i = 0, idx = 0; i < ldu_matrix.cells; ++i) {
+        while (col_end[idx] <= i) {
+            idx += 1;
+        }
+        bscache[i] = idx;
+        block_size[idx * result.chunk_num + idx] += 1;
     }
 
-    for (int i = 0; i < ldu_matrix.cells; ++i) {
-        int idx = cached_binary_search_single(
-            col_begin,
-            col_end,
-            result.chunk_num,
-            i);
-        block_size[idx * result.chunk_num + idx] += 1;
+    for (int i = 0; i < ldu_matrix.faces; ++i) {
+        int chunk_idx = bscache[ldu_matrix.uPtr[i]];
+        int block_idx = bscache[ldu_matrix.lPtr[i]];
+        block_size[chunk_idx * result.chunk_num + block_idx] += 1;
+        block_size[block_idx * result.chunk_num + chunk_idx] += 1;
     }
 
     memset(chunk_block_off, 0, sizeof(int) * chunk_num * chunk_num);
@@ -430,28 +283,26 @@ ldu_to_splited_coo(const LduMatrix &ldu_matrix, int chunk_num) {
     }
 
     for (int i = 0; i < ldu_matrix.faces; ++i) {
-        int idx1 = BSCache.data[ldu_matrix.uPtr[i]];
-        int idx2 = BSCache.data[ldu_matrix.lPtr[i]];
+        int idx1 = bscache[ldu_matrix.uPtr[i]];
+        int idx1_offset = ldu_matrix.uPtr[i] - col_begin[idx1];
+        int idx2 = bscache[ldu_matrix.lPtr[i]];
+        int idx2_offset = ldu_matrix.lPtr[i] - col_begin[idx2];
         int idx_21 = chunk_block_off[idx2 * result.chunk_num + idx1];
         chunk_block_off[idx2 * result.chunk_num + idx1] += 1;
         int idx_12 = chunk_block_off[idx1 * result.chunk_num + idx2];
         chunk_block_off[idx1 * result.chunk_num + idx2] += 1;
 
-        result.chunks[idx2].chunk->row_idx[idx_21] =
-            ldu_matrix.lPtr[i] - col_begin[idx2];
-        result.chunks[idx2].chunk->col_idx[idx_21] =
-            ldu_matrix.uPtr[i] - col_begin[idx1];
-        result.chunks[idx2].chunk->data[idx_21] = ldu_matrix.upper[i];
-
-        result.chunks[idx1].chunk->row_idx[idx_12] =
-            ldu_matrix.uPtr[i] - col_begin[idx1];
-        result.chunks[idx1].chunk->col_idx[idx_12] =
-            ldu_matrix.lPtr[i] - col_begin[idx2];
+        result.chunks[idx1].chunk->row_idx[idx_12] = idx1_offset;
+        result.chunks[idx1].chunk->col_idx[idx_12] = idx2_offset;
         result.chunks[idx1].chunk->data[idx_12] = ldu_matrix.lower[i];
+
+        result.chunks[idx2].chunk->row_idx[idx_21] = idx2_offset;
+        result.chunks[idx2].chunk->col_idx[idx_21] = idx1_offset;
+        result.chunks[idx2].chunk->data[idx_21] = ldu_matrix.upper[i];
     }
 
     for (int i = 0; i < ldu_matrix.cells; ++i) {
-        int id = BSCache.data[i];
+        int id = bscache[i];
         auto &chunk = *result.chunks[id].chunk;
         auto &idx = chunk_block_off[id * result.chunk_num + id];
         chunk.row_idx[idx] = i - col_begin[id];
